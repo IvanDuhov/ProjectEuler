062729e99f80da9908c923a30c1082392723ec95
unknown <duhovgames@gmail.com>
2019-03-05 22:19:55 +0200

    p03

---
+++ p03.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjectEuler
+{
+    class p01
+    {
+        static void Main(string[] args)
+        {
+            long n = 600851475143;
+            long k = 1;
+
+            List<long> factors = new List<long>();
+
+            long res = 0;
+
+            while (k * k < n)
+            {
+                if (n % k == 0)
+                {
+                    factors.Add(k);
+                }
+                
+                k++;
+            }
+
+            for (int i = 0; i < factors.Count; i++)
+            {
+                res = factors.Max();
+                bool isPrime = true;
+
+                for (int j = 2; j < res / 2; j++)
+                {
+                    if (res % j == 0)
+                    {
+                        isPrime = false;
+                        factors.Remove(res);
+                        break;
+                    }
+                }
+
+                if (isPrime)
+                {
+                    Console.WriteLine($"The largest prime factor is: {res}");
+                    break;
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

64556d8ff3d8de3a9bd7aa2f6b85bd7bca5826bc
unknown <duhovgames@gmail.com>
2019-03-28 22:29:10 +0200

    problem uploaded

---
+++ p06.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjectEuler
+{
+    class p06
+    {
+        static void Main(string[] args)
+        {
+            int sumOfSquares = 0;
+            uint squareOfTheSum = 0;
+
+            for (int i = 1; i < 101; i++)
+            {
+                sumOfSquares += i * i;
+            }
+
+            for (int i = 1; i < 101; i++)
+            {
+                squareOfTheSum += uint.Parse(i.ToString());
+            }
+            squareOfTheSum *= squareOfTheSum;
+
+            Console.WriteLine(squareOfTheSum - sumOfSquares);
+            Console.ReadKey();
+        }
+    }
+}

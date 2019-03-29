89c1409bb47b9aae25c2bba2fc6ad56817b6de12
unknown <duhovgames@gmail.com>
2019-03-29 17:00:07 +0200

    problem solved

---
+++ p09.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ProjectEuler
+{
+    class p09
+    {
+        static void Main(string[] args)
+        {
+            for (int a = 0; a < 999; a++)
+            {
+                for (int b = 0; b < 998; b++)
+                {
+                    for (int c = 0; c < 997; c++)
+                    {
+                        if (a < b && b < c)
+                        {
+                            if ((a * a + b * b == c * c) && (a + b + c == 1000))
+                            {
+                                Console.WriteLine(a * b * c);
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

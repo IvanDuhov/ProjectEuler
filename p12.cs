0f46c94d611463842555e9cee9267580b847d310
Ivan Duhov <duhovgames@gmail.com>
2019-06-15 19:08:37 +0300

    problem solved
    The sequence of triangle numbers is generated by adding the natural numbers. So the 7th triangle number would be 1 + 2 + 3 + 4 + 5 + 6 + 7 = 28. The first ten terms would be:
    
    1, 3, 6, 10, 15, 21, 28, 36, 45, 55, ...
    
    Let us list the factors of the first seven triangle numbers:
    
     1: 1
     3: 1,3
     6: 1,2,3,6
    10: 1,2,5,10
    15: 1,3,5,15
    21: 1,3,7,21
    28: 1,2,4,7,14,28
    We can see that 28 is the first triangle number to have over five divisors.
    
    What is the value of the first triangle number to have over five hundred divisors?

---
+++ p12.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectEuler
+{
+    class p12
+    {
+        public static void Main()
+        {
+            uint index = 1;
+            long num = 1;
+            Console.WriteLine(Divisors(16));
+
+            while (!(Divisors(num) > 500))
+            {
+                index++;
+                num += index;
+                Console.WriteLine(num);
+            }
+
+            Console.WriteLine(num);
+        }
+
+        public static uint Divisors(long num)
+        {
+            uint res = 0;
+            uint sqrt = (uint)(Math.Sqrt(num));
+
+            for (int i = 1; i <= sqrt; i++)
+            {
+                if (num % i == 0)
+                {
+                    res += 2;
+                }
+            }
+
+            // if the number is a perfect square we need to remove 2 divisors
+            if (sqrt * sqrt == num)
+            {
+                res -= 2;
+            }
+
+            return res;
+        }
+    }
+
+}
